a9eb093ff9ddab243d89861ac2b6a199794dbee9
Vrunda <vrundak@bsf.io>
2021-12-07 10:51:47 +0530

    Deleted dist build directory

--- inc/lib/onboarding/assets/dist/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n', 'wp-media-utils', 'wp-polyfill', 'wp-url'), 'version' => 'efbb95e100c5ee663268d682410df5e5');
