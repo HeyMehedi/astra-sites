55d926efebea79008d366524f438b337265435b4
Vrunda <vrundak@bsf.io>
2021-12-14 17:24:42 +0530

    Release ready and changelog

--- inc/lib/onboarding/assets/src/steps/import-site/index.js
+++ inc/lib/onboarding/assets/src/steps/import-site/index.js
@@ -1363,6 +1363,20 @@ const ImportSite = () => {
 		templateResponse,
 	] );
 
+	const preventRefresh = ( event ) => {
+		event.returnValue = __(
+			'Are you sure you want to cancel the site import process?',
+			'astra-sites'
+		);
+		return event;
+	};
+
+	useEffect( () => {
+		window.addEventListener( 'beforeunload', preventRefresh ); // eslint-disable-line @wordpress/no-global-event-listener
+		return () =>
+			window.removeEventListener( 'beforeunload', preventRefresh ); // eslint-disable-line @wordpress/no-global-event-listener
+	} );
+
 	// Start the import process.
 	useEffect( () => {
 		/**
